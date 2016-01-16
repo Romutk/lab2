c719080293157178cc51cd97b55027acb3b40a3f
Roma <newangel88@mail.ru>
2016-01-16 22:10:18 +0400

    поменял программу, под библиотеку в кторой будет две функции

--- envx.c
+++ envx.c
@@ -9,9 +9,9 @@ int main(int argc, char const *argv[])
 {
 if( argc<2 )  return 1;
 char output[256];
-char end_path="/environ";
+
 //int result = funxxx();
-funxxx(argv[1],end_path, output, sizeof(output));
+funenvx(argv[1], output, sizeof(output));
 printf ("%s\n", output);
 return 0;
 }
